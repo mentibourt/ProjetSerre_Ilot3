5755747d438b6e76db872ac90349fb0446a53043
ejany <ejany@btslivh.eu>
2019-01-15 17:26:52 +0100

    Création projet SuperviseurSerre

---
+++ SuperviseurSerre/newForm.cpp.cc
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   newForm.cpp
+ * Author: snir2g1
+ *
+ * Created on 15 janvier 2019, 17:23
+ */
+
+#include "newForm.h"
+
+newForm::newForm() {
+    widget.setupUi(this);
+}
+
+newForm::~newForm() {
+}
