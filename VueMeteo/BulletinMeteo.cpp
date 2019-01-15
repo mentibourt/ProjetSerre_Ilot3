d14f5219bd41450b3c0823ef3ef6c25971950ba5
frenaud <frenaud@btslivh.eu>
2019-01-15 17:10:17 +0100

    transfert Bulletin meteo

---
+++ VueMeteo/BulletinMeteo.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   BulletinMeteo.cpp
+ * Author: snir2g1
+ * 
+ * Created on 15 janvier 2019, 16:51
+ */
+
+#include "BulletinMeteo.h"
+
+BulletinMeteo::BulletinMeteo() {
+}
+
+BulletinMeteo::BulletinMeteo(const BulletinMeteo& orig) {
+}
+
+BulletinMeteo::~BulletinMeteo() {
+}
+
--- VueMeteo/nbproject/qt-Debug.mk
+++ VueMeteo/nbproject/qt-Debug.mk
@@ -33,7 +33,7 @@ MOVE          = mv -f
 TAR           = tar -cf
 COMPRESS      = gzip -9f
 DISTNAME      = VueMeteo1.0.0
-DISTDIR = /home/snir2g1/NetBeansProjects/VueMeteo/build/Debug/GNU-Linux/VueMeteo1.0.0
+DISTDIR = /home/snir2g1/Documents/tpSerre/ProjetSerre_Ilot3/VueMeteo/build/Debug/GNU-Linux/VueMeteo1.0.0
 LINK          = g++
 LFLAGS        = -Wl,-rpath,/opt/Qt/5.7/gcc_64/lib
 LIBS          = $(SUBLIBS) -L/opt/Qt/5.7/gcc_64/lib -lQt5Widgets -L/usr/lib64 -lQt5Gui -lQt5Core -lGL -lpthread 
@@ -48,9 +48,11 @@ OBJECTS_DIR   = build/Debug/GNU-Linux/
 
 ####### Files
 
-SOURCES       = VueMeteo.cpp.cc \
+SOURCES       = BulletinMeteo.cpp \
+		VueMeteo.cpp.cc \
 		main.cpp moc_VueMeteo.cpp
-OBJECTS       = build/Debug/GNU-Linux/VueMeteo.cpp.o \
+OBJECTS       = build/Debug/GNU-Linux/BulletinMeteo.o \
+		build/Debug/GNU-Linux/VueMeteo.cpp.o \
 		build/Debug/GNU-Linux/main.o \
 		build/Debug/GNU-Linux/moc_VueMeteo.o
 DIST          = /opt/Qt/5.7/gcc_64/mkspecs/features/spec_pre.prf \
@@ -201,7 +203,9 @@ DIST          = /opt/Qt/5.7/gcc_64/mkspecs/features/spec_pre.prf \
 		/opt/Qt/5.7/gcc_64/mkspecs/features/exceptions.prf \
 		/opt/Qt/5.7/gcc_64/mkspecs/features/yacc.prf \
 		/opt/Qt/5.7/gcc_64/mkspecs/features/lex.prf \
-		nbproject/nbproject/qt-Debug.pro VueMeteo.h VueMeteo.cpp.cc \
+		nbproject/nbproject/qt-Debug.pro BulletinMeteo.h \
+		VueMeteo.h BulletinMeteo.cpp \
+		VueMeteo.cpp.cc \
 		main.cpp
 QMAKE_TARGET  = VueMeteo
 DESTDIR       = dist/Debug/GNU-Linux/
@@ -534,8 +538,8 @@ dist: distdir FORCE
 distdir: FORCE
 	@test -d $(DISTDIR) || mkdir -p $(DISTDIR)
 	$(COPY_FILE) --parents $(DIST) $(DISTDIR)/
-	$(COPY_FILE) --parents VueMeteo.h $(DISTDIR)/
-	$(COPY_FILE) --parents VueMeteo.cpp.cc main.cpp $(DISTDIR)/
+	$(COPY_FILE) --parents BulletinMeteo.h VueMeteo.h $(DISTDIR)/
+	$(COPY_FILE) --parents BulletinMeteo.cpp VueMeteo.cpp.cc main.cpp $(DISTDIR)/
 	$(COPY_FILE) --parents VueMeteo.ui $(DISTDIR)/
 
 
@@ -713,7 +717,7 @@ moc_VueMeteo.cpp: ui_VueMeteo.h \
 		/opt/Qt/5.7/gcc_64/include/QtWidgets/QWidget \
 		VueMeteo.h \
 		/opt/Qt/5.7/gcc_64/bin/moc
-	/opt/Qt/5.7/gcc_64/bin/moc $(DEFINES) -I/opt/Qt/5.7/gcc_64/mkspecs/linux-g++ -I/home/snir2g1/NetBeansProjects/VueMeteo/nbproject -I/opt/Qt/5.7/gcc_64/include -I/opt/Qt/5.7/gcc_64/include/QtWidgets -I/opt/Qt/5.7/gcc_64/include/QtGui -I/opt/Qt/5.7/gcc_64/include/QtCore -I. -I/usr/include/c++/6.1.1 -I/usr/include/c++/6.1.1/x86_64-redhat-linux -I/usr/include/c++/6.1.1/backward -I/usr/lib/gcc/x86_64-redhat-linux/6.1.1/include -I/usr/local/include -I/usr/include VueMeteo.h -o moc_VueMeteo.cpp
+	/opt/Qt/5.7/gcc_64/bin/moc $(DEFINES) -I/opt/Qt/5.7/gcc_64/mkspecs/linux-g++ -I/home/snir2g1/Documents/tpSerre/ProjetSerre_Ilot3/VueMeteo/nbproject -I/opt/Qt/5.7/gcc_64/include -I/opt/Qt/5.7/gcc_64/include/QtWidgets -I/opt/Qt/5.7/gcc_64/include/QtGui -I/opt/Qt/5.7/gcc_64/include/QtCore -I. -I/usr/include/c++/6.1.1 -I/usr/include/c++/6.1.1/x86_64-redhat-linux -I/usr/include/c++/6.1.1/backward -I/usr/lib/gcc/x86_64-redhat-linux/6.1.1/include -I/usr/local/include -I/usr/include VueMeteo.h -o moc_VueMeteo.cpp
 
 compiler_moc_source_make_all:
 compiler_moc_source_clean:
@@ -733,6 +737,9 @@ compiler_lex_clean:
 compiler_clean: compiler_moc_header_clean compiler_uic_clean 
 
 ####### Compile
+
+build/Debug/GNU-Linux/BulletinMeteo.o: BulletinMeteo.cpp BulletinMeteo.h
+	$(CXX) -c $(CXXFLAGS) $(INCPATH) -o build/Debug/GNU-Linux/BulletinMeteo.o BulletinMeteo.cpp
 
 build/Debug/GNU-Linux/VueMeteo.cpp.o: VueMeteo.cpp.cc VueMeteo.h \
 		ui_VueMeteo.h \
